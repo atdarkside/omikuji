7c8afb97ef05a3df0cd5f0241c0633cb416849b8
TATa <pf35301@gmail.com>
2016-05-22 23:34:42 +0900

    変数による実行を追加

--- Program.cs
+++ Program.cs
@@ -6,12 +6,31 @@ using System.Threading.Tasks;
 
 namespace omikuji {
     class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("10連おみくじ");
 
+
+
+        static void Main(string[] args) {
+            string[] results = new string[] {
+                    "大吉",
+                    "中吉",
+                    "小吉",
+                    "凶",
+                    "大凶" };
+            Random r = new Random();
+            int choice;
+
+            do {
+                Console.WriteLine("どちらの方法で実行するか指定してください。");
+                Console.WriteLine("変数:0");
+                Console.WriteLine("三項演算子:1");
+               choice = int.Parse(Console.ReadLine());
+            } while(choice >= 1 && choice < 0);
+
+            Console.WriteLine("10連おみくじ");
             for(int i = 0; i <= 10; i++) {
                 Console.ReadKey();
-                Console.WriteLine("おみくじの結果は{0}です。", omikuji());
+                if(choice == 0) Console.WriteLine("おみくじの結果は{0}です。",results[r.Next(5)]);
+                else Console.WriteLine("おみくじの結果は{0}です。",omikuji());
             }
             Console.WriteLine("終了");
             Console.ReadKey();
